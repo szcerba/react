b8909d63bd321305407949ba05f9e66c8bb1c184
szcer <szcerba@gmail.com>
2024-01-19 11:24:00 -0300

    update repo with third project

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css'
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  const handleMove = (e) => {
+    const { clientX, clientY } = e
+    setPosition({ x: clientX, y: clientY })
+  }
+
+  useEffect(() => {
+    enabled ? window.addEventListener('pointermove', handleMove) : ''
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button>
+    </>
+  )
+}
+
+function App () {
+
+  return (
+    <main>
+      <FollowMouse/>
+    </main>
+  )
+}
+
+export default App
